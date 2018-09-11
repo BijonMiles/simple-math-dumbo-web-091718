37e74ff972149d299cc4333e1787db99837b6c93
Bijon Miles <bijonmiles01@gmail.com>
2018-09-11 07:49:16 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  sum = num1 + num2
 end
 
 def subtraction(num1, num2)
+  sub = num2 - num1
 end
 
 def division(num1, num2)
+  div = num2/num1
 end
 
 def multiplication(num1, num2)
+  mul = num1*num2
 end
 
 def modulo(num1, num2)
+  rem = num2 % num1
 end
 
-def square_root(num)
+def square_root
+  Math.sqrt(100)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  oper = num4 + ((num1*num3)/num2)
 end
